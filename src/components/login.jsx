1eb1db9fdbe9b8ef9fea82e22e2c4db96df2190f
RazCrimson3 <bharathvigneshjo@gmail.com>
2019-12-13 01:40:31 +0530

    Added basic Event handlers' outline

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,78 +3,150 @@ import Container from "@material-ui/core/Container";
 import "./Login.css";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
+	constructor(props) {
+		super(props);
+		this.state = { isToggleOn: true };
+		this.handleSignInSubmit = this.handleSignInSubmit.bind(this);
+		this.handleSignUpSubmit = this.handleSignUpSubmit.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
-  }
-  render() {
-     return (
-      <Container component="main" maxWidth="xs">
-        <div className={this.state.isToggleOn?"container":"container right-panel-active"} id="container">
-	<div className="form-container sign-up-container">
-		<form action="#">
-			<h1>Create Account</h1>
-			<div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div className="form-container sign-in-container">
-		<form action="#">
-			<h1>Sign in</h1>
-			<div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div className="overlay-container">
-		<div className="overlay">
-			<div className="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button className="ghost" onClick={this.handleClick} id="signIn">Sign In</button>
-			</div>
-			<div className="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button className="ghost" onClick={this.handleClick} id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
+	handleClick() {
+		this.setState(state => ({
+			isToggleOn: !state.isToggleOn
+		}));
+	}
 
-<footer>
-	<p>
-		Created with <i className="fa fa-heart"></i> by
-		<a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-		- Read how I created this and how you can join the challenge
-		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-	</p>
-</footer>
-      </Container>
-    );
-  }
+	handleErrors(response) {
+		if (!response.ok) {
+			throw Error(response.statusText);
+			// Make a Error Popup
+		}
+		return response;
+	}
+
+	handleSignInSubmit(event) {
+		event.preventDefault();
+		const data = new FormData(event.target);
+
+		fetch("/user/sign-in", {
+			method: "POST",
+			body: data
+		})
+		.then(this.handleErrors)
+		.then(response => console.log("Signed In"))
+		.catch(error => console.log(error));
+	}
+
+	handleSignUpSubmit(event) {
+		event.preventDefault();
+		const data = new FormData(event.target);
+
+		if (data.get("registerPassword") !== data.get("conPassword")) {
+			// Raise Password Mismatch
+			// Possibly use a Dynamic way to Update and render it
+		}
+
+		if (data.get("registerPassword").length < 8 || data.get("registerName").length < 8) {
+			// Raise Length error
+		}
+
+		fetch("/user/sign-in", {
+			method: "POST",
+			body: data
+		})
+		.then(this.handleErrors)
+		.then(response => console.log("Signed Up"))
+		.catch(error => console.log(error));
+	}
+
+	render() {
+		return (
+			<Container component="main" maxWidth="xs">
+				<div
+					className={
+						this.state.isToggleOn ? "container" : "container right-panel-active"
+					}
+					id="container"
+				>
+					<div className="form-container sign-up-container">
+						<form onSubmit={this.handleSignUpSubmit}>
+							<h1>Create Account</h1>
+							<div className="social-container">
+								<a href="#" className="social">
+									<i className="fab fa-facebook-f"></i>
+								</a>
+								<a href="#" className="social">
+									<i className="fab fa-google-plus-g"></i>
+								</a>
+								<a href="#" className="social">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
+							</div>
+							<span>or use your email for registration</span>
+							<input type="text" id="registerName" placeholder="Username" />
+							<input type="password" id="registerPassword" placeholder="Password" />
+							<input
+								type="password"
+								id="confirmPassword"
+								placeholder="Confirm Password"
+							/>
+							<button>Sign Up</button>
+						</form>
+					</div>
+					<div className="form-container sign-in-container">
+						<form onSubmit={this.handleSignInSubmit}>
+							<h1>Sign in</h1>
+							<div className="social-container">
+								<a href="#" className="social">
+									<i className="fab fa-facebook-f"></i>
+								</a>
+								<a href="#" className="social">
+									<i className="fab fa-google-plus-g"></i>
+								</a>
+								<a href="#" className="social">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
+							</div>
+							<span>or use your account</span>
+							<input type="text" id="username" placeholder="Username" />
+							<input type="password" id="password" placeholder="Password" />
+							<a href="/forgotPassword.html">Forgot your password?</a>
+							<button action="submit">Sign In</button>
+						</form>
+					</div>
+					<div className="overlay-container">
+						<div className="overlay">
+							<div className="overlay-panel overlay-left">
+								<h1>Welcome Back!</h1>
+								<p>
+									To keep connected with us please login with your personal info
+								</p>
+								<button
+									className="ghost"
+									onClick={this.handleClick}
+									id="signIn"
+								>
+									Sign In
+								</button>
+							</div>
+							<div className="overlay-panel overlay-right">
+								<h1>Hello, Friend!</h1>
+								<p>Enter your personal details and start journey with us</p>
+								<button
+									className="ghost"
+									onClick={this.handleClick}
+									id="signUp"
+								>
+									Sign Up
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Container>
+		);
+	}
 }
 
 export default Login;
